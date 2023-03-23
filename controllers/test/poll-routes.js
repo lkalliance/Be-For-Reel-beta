ff1e146f9a9e1ae69c8656a46e29b87b707a3281
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-23 17:03:02 -0500

    Updated CSS and page structure

--- controllers/test/poll-routes.js
+++ controllers/test/poll-routes.js
@@ -11,26 +11,7 @@ router.get('/create', async (req, res) => {
       userId: req.session.userId,
       loggedIn: req.session.loggedIn
     }
-    const css = { url: '/css/create.css' }
-
-    res.render('createpoll', { userInfo, css });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get('/search/:string', async (req, res) => {
-  // Route to get movies by title search
-  try {
-console.log(`searching by ${req.params.string}`);
-    const options = {
-      method: 'GET',
-      url: `https://imdb-api.com/API/AdvancedSearch/${process.env.IMDB_API_KEY}?title=${req.params.string}&title_type=feature`
-    };
-    const movieData = await fetch.request(options);
-    res.status(200).json(movieData.data);
-
+    res.render('createpoll', { userInfo });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
