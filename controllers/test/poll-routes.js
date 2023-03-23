e2a74f9f188da61739e000e567be113dcf061721
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-23 15:59:36 -0500

    Cleaned up search layout

--- controllers/test/poll-routes.js
+++ controllers/test/poll-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const isAuth = require('../../utils/auth');
+const { Poll } = require('../../models');
 
 
-router.get('/', async (req, res) => {
+router.get('/create', async (req, res) => {
   // Sample
   try {
     const userInfo = {
@@ -10,7 +11,26 @@ router.get('/', async (req, res) => {
       userId: req.session.userId,
       loggedIn: req.session.loggedIn
     }
-    res.render('createpoll', { userInfo });
+    const css = { url: '/css/create.css' }
+
+    res.render('createpoll', { userInfo, css });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/search/:string', async (req, res) => {
+  // Route to get movies by title search
+  try {
+console.log(`searching by ${req.params.string}`);
+    const options = {
+      method: 'GET',
+      url: `https://imdb-api.com/API/AdvancedSearch/${process.env.IMDB_API_KEY}?title=${req.params.string}&title_type=feature`
+    };
+    const movieData = await fetch.request(options);
+    res.status(200).json(movieData.data);
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
