21f7da0b67de85914e9fc5f083718bd900208974
Justin-Brueske <119259409+Justin-Brueske@users.noreply.github.com>
2023-03-23 19:07:04 -0500

    Update user-routes.js
    user login routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -5,16 +5,68 @@ const { User, Movie, Poll, Vote, Comment } = require('../../models');
 router.post('/', async (req, res) => {
   // Route to create a new user
   try {
+    const dbUserData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.username = dbUserData.username
+      req.session.loggedIn = true;
+      res.status(200).json(dbUserData);
+      console.log('logged in', req.session);
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+// Login
 router.post('/login', async (req, res) => {
-  console.log(req.body);
-  // CHANGE THE ROUTE HERE TO WHATEVER 
-  res.sendStatus(200);
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again.'});
+      return;
+    }
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password. Please try again.'});
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.username = dbUserData.username
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: 'You are logged in.'});
+      console.log('logged in', req.session);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+
+// Logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 
 module.exports = router;
