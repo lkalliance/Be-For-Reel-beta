35f6455f8c5b4fe8423576537fd256e05c9dec16
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-25 14:36:26 -0500

    Did various things: --Corrected navbar logic --Moved create poll page to live --Fixed logout function

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const pollRoutes = require('./poll-routes');
 const testRoutes = require('./test');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/polls', pollRoutes);
 router.use('/test', testRoutes);
 
 module.exports = router;
