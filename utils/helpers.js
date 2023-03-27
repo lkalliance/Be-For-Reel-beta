ed204e8dd211ebf3ac8ea8299147056778641b6e
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-27 16:45:12 -0500

    Fixed order of poll listing, and date formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,6 @@ module.exports = {
     return date.toLocaleTimeString();
   },
   format_date: (date) => {
-    return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   }
 };
