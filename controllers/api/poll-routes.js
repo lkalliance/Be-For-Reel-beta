4a07f5d56bb4697d74b8a3c0645f738aca8f3c0b
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-03-27 22:36:16 -0500

    Don't know how it broke, but I fixed it.

--- controllers/api/poll-routes.js
+++ controllers/api/poll-routes.js
@@ -5,29 +5,40 @@ const { User, Movie, Poll, Vote, Comment, Opt } = require('../../models');
 
 
 router.post('/create', withAuth, async (req, res) => {
-    try {
-        // creates or finds movies returns ids
-        const filmList = [];
-        for (film of req.body.films) {
-            const temp = await Movie.findOrCreate({
-                where: { imdb_id: film.imdb_id },
-                defaults: {
-                    image: film.image,
-                    title: film.title,
-                }
-            })
-            filmList.push(temp[0].dataValues.id);
-         }
+  try {
+      // creates or finds movies returns ids
+      const filmList = [];
+      for (film of req.body.films) {
+          const temp = await Movie.findOrCreate({
+              where: { imdb_id: film.imdb_id },
+              defaults: {
+                  image: film.image,
+                  title: film.title,
+              }
+          })
+          filmList.push(temp[0].dataValues.id);
+       }
 
-        // creates the poll
-        const newPoll = await Poll.create({
-            title: req.body.title,
-            description: req.body.desc,
-            user_id: req.session.userId,
-        });
-const router = require('express').Router();
-const isAuth = require('../utils/auth');
-const { Poll, Opt, User, Movie, Vote } = require('../models');
+      // creates the poll
+      const newPoll = await Poll.create({
+          title: req.body.title,
+          description: req.body.desc,
+          user_id: req.session.userId,
+      });
+      
+      // creates the options for polls
+      for (opts of filmList) {
+          const temp = await Opt.create({
+              poll_id: newPoll.dataValues.id,
+              movie_id: opts,
+          })
+      }
+
+      res.json(newPoll.dataValues.id);
+  }  catch (err) {
+      res.status(500).json(err);
+  }
+});
 
 router.get('/vote/:id', async (req, res) => {
   // Sample
@@ -64,7 +75,7 @@ router.get('/vote/:id', async (req, res) => {
 
 
 
-router.get('/create', isAuth, async (req, res) => {
+router.get('/create', withAuth, async (req, res) => {
   try {
     const userInfo = {
       username: req.session.username,
@@ -79,21 +90,6 @@ router.get('/create', isAuth, async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
-});
-        
-
-        // creates the options for polls
-        for (opts of filmList) {
-            const temp = await Opt.create({
-                poll_id: newPoll.dataValues.id,
-                movie_id: opts,
-            })
-        }
-
-        res.json(newPoll.dataValues.id);
-    }  catch (err) {
-        res.status(500).json(err);
-    }
 });
 
 
